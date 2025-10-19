6bfdb514a144cad8ef064337b4ed84cbf15c2229
Muhammad Safwan <mohammadsafwan248@gmail.com>
2025-10-19 17:14:20 +0500

    add more functionality to menu

--- littleLemon/restraunt/urls.py
+++ littleLemon/restraunt/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('menu/', views.menu, name='Menu'),
+    path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
 ]
+
--- littleLemon/restraunt/views.py
+++ littleLemon/restraunt/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import MenuItem
+from django.shortcuts import redirect, get_object_or_404
 
 def home(request):
     return render(request, 'home.html')
@@ -13,3 +14,10 @@ def menu(request):
     menu_items = MenuItem.objects.filter(is_available=True)
     context = {'menu_items': menu_items}
     return render(request, 'menu.html', context)
+
+def add_to_cart(request, item_id):
+    # Example: simple session-based cart
+    cart = request.session.get('cart', {})
+    cart[item_id] = cart.get(item_id, 0) + 1
+    request.session['cart'] = cart
+    return redirect('menu')  # redirect back to menu page
